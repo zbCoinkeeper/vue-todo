172d0c1fac2709766cc319bb84270ba32aaf0716
zb <2479107942@qq.com>
2019-03-31 22:36:10 +0800

    修改透明度

--- src/todo/todo.vue
+++ src/todo/todo.vue
@@ -89,7 +89,7 @@ export default {
     font-smoothing antialiased
     padding 16px 16px 16px 36px
     border none
-    box-shadow inset 0 -2px 1px rgba(0, 0, 0, 0.03)
+    box-shadow inset 0 -2px 1px rgba(0, 0, 0, 0.05)
     
 </style>
 
